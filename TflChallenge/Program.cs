1618b98e7c5bf026e9fb6d2ea1dc421a2aa3b103
Mark Dickens <mark.dickens83@gmail.com>
2018-02-23 08:42:56 +0000

    Created basic working api call.  Added NewtonSoft.Json dependency

--- TflChallenge/Program.cs
+++ TflChallenge/Program.cs
@@ -1,15 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Tfl.Api.Presentation.Entities;
 
 namespace TflChallenge
 {
     class Program
     {
+        static HttpClient client = new HttpClient();
+
         static void Main(string[] args)
         {
+            var id = args[0];
+
+            var response = callRoadApi(id).GetAwaiter().GetResult();
+            Console.WriteLine(response.DisplayName);
+            Console.ReadLine();
+        }
+
+        static async Task<RoadCorridor> callRoadApi(string id)
+        {
+            client.BaseAddress = new Uri($"https://api.tfl.gov.uk/Road/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var app_id = "";
+            var app_key = "";
+
+            var response = await client.GetAsync($"{id}?app_id={app_id}&app_key={app_key}");
+
+            var result = await response.Content.ReadAsStringAsync();
+            var roadCorridors = JsonConvert.DeserializeObject<RoadCorridor[]>(result);
+            return roadCorridors.FirstOrDefault();
         }
     }
 }
