d4c364ded927f7c893de342aba4bd82eeab05d0d
Mark Dickens <mark.dickens83@gmail.com>
2018-02-25 14:16:08 +0000

    Updated Main method to write the road status to console.  Added ExitCode enum.

---
+++ TflChallenge/Models/ExitCode.cs
@@ -0,0 +1,10 @@
+﻿namespace TflChallenge.Models
+{
+    public enum ExitCode
+    {
+        Success = 0,
+        Error_InvalidArgs,
+        Error_EntityNotFound,
+        Error_UnexpectedError
+    }
+}
--- TflChallenge/Program.cs
+++ TflChallenge/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using TflChallenge.API;
+using TflChallenge.Models;
 using TflChallenge.Services;
 
 namespace TflChallenge
@@ -11,15 +12,38 @@ namespace TflChallenge
 
         static void Main(string[] args)
         {
+            validateArgs(args);
             var id = args[0];
 
             var api = new RoadApi(client);
             var roadService = new RoadService(api);
-
             var response = roadService.GetRoadStatusById(id).GetAwaiter().GetResult();
 
-            Console.WriteLine(response.RoadDisplayName);
-            Console.ReadLine();
+            if (!response.Success)
+            {
+                Console.WriteLine($"An unexpected error occurred whilst retrieving the status for road with Id {id}");
+                Environment.Exit((int)ExitCode.Error_UnexpectedError);
+            }
+
+            if (response.RoadStatus == null)
+            {
+                Console.WriteLine($"{id} is not a valid road");
+                Environment.Exit((int)ExitCode.Error_EntityNotFound);
+            }
+
+            Console.WriteLine($"The status of the {response.RoadId} is as follows");
+            Console.WriteLine($"\t Road Status is {response.RoadStatus.StatusSeverity}");
+            Console.WriteLine($"\t Road Status Description is {response.RoadStatus.StatusSeverityDescription}");
+            Environment.Exit((int)ExitCode.Success);
+        }
+
+        private static void validateArgs(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Please provide a valid road ID (e.g. A2 or \"Blackwall Tunnel\")");
+                Environment.Exit((int)ExitCode.Error_InvalidArgs);
+            }
         }
     }
 }
